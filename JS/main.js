fcc403190e874d18bf8d0b1b359574913a2c97cc
AndreaPiazza33 <andreapiazza190201@gmail.com>
2023-07-21 16:30:33 +0200

    feat: add numbers

---
+++ JS/main.js
@@ -0,0 +1,11 @@
+//creo la lista 
+//creo il modulo per definire i multipli 
+//associo ad essi le parole 
+
+
+//ciclo che crea i numeri da 1 a 100
+for (let i = 1; i < 101; i++){
+   console.log(i); 
+}
+
+
