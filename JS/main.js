d9fa3b7f10c50d975d191eea4b5f5bcd642add3f
AndreaPiazza33 <andreapiazza190201@gmail.com>
2023-07-21 17:34:28 +0200

    feat: add cycle variables

--- JS/main.js
+++ JS/main.js
@@ -2,7 +2,9 @@
 //creo il modulo per definire i multipli 
 //associo ad essi le parole 
 
-
+let a = "FizzBuzz"
+let b = "Fizz"
+let c = "Buzz"
 //ciclo che crea i numeri da 1 a 100
 for (let i = 1; i <= 100; i++){
 
